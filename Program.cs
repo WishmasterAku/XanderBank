41333e168cc70c08c7346432e8e949c1a6083bbe
WishmasterAku <WishmasterAkunin@gmail.com>
2022-07-12 00:10:33 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Customer
+{
+    string? cardNumber;
+    int pinNum;
+    string? firstName;
+    string? lastName;
+    double balance;
+    public Customer(string? cardNumber, int pinNum, string? firstName, string? lastName, double balance)
+    {
+        this.cardNumber = cardNumber;
+        this.pinNum = pinNum;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+    public String? getNum()
+    {
+        return cardNumber;
+    }
+    public int getPin()
+    {
+        return pinNum;
+    }
+    public String? getFirstName()
+    {
+        return firstName;
+    }
+    public String? getLastName()
+    {
+        return lastName;
+    }
+    public double getBalance()
+    {
+        return balance;
+    }
+    public void setNum(String? newCardNumber)
+    {
+        cardNumber = newCardNumber;
+    }
+    public void setPin(int newPinNum)
+    {
+        pinNum = newPinNum;
+    }
+    public void setFirstName(string? newFirstName)
+    {
+        firstName = newFirstName;
+    }
+    public void setLastName(string? newLastName)
+    {
+        lastName = newLastName;
+    }
+    public void setBalance(double newBalance)
+    {
+        balance = newBalance;
+    }
+    public static void Main(String[] args)
+    {
+
+    }
+
+}
